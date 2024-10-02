430fbfb036dda1bea56f1d65647747e92e279bf3
fuadmhnr <fuadmhnr@gmail.com>
2024-10-02 14:46:46 +0700

    feat: navigation bar

---
+++ resources/views/components/navigation-menu.blade.php
@@ -0,0 +1,19 @@
+<nav class="navbar navbar-expand-lg bg-body-tertiary">
+    <div class="container">
+      <a class="navbar-brand" href="#">
+        {{ config('app.name') }}
+      </a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+          <x-nav-link href="{{ route('home') }}" :active="request()->routeIs('home')">Home</x-nav-link> 
+          <x-nav-link href="{{ route('about') }}" :active="request()->routeIs('about')">About</x-nav-link> 
+          <x-nav-link href="{{ route('contact') }}" :active="request()->routeIs('contact')">Contact</x-nav-link> 
+          <x-nav-link href="{{ route('posts.index') }}" :active="request()->routeIs('posts.index')">Posts</x-nav-link> 
+
+        </ul>
+      </div>
+    </div>
+  </nav>
