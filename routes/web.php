9c88c019c51fe245bf8b1a86944b874b0b09e29c
fuadmhnr <fuadmhnr@gmail.com>
2024-10-02 13:34:50 +0700

    feat: layout and dynamic title

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
+use App\Livewire;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', Livewire\Home::class)->name('home');
+Route::get('/about', Livewire\About::class)->name('about');
+Route::get('/contact', Livewire\Contact::class)->name('contact');
+Route::get('/posts', Livewire\Posts\Index::class)->name('posts.index');
+
+Route::get('/users/{user}', Livewire\Users\Show::class)->name('users.show');
